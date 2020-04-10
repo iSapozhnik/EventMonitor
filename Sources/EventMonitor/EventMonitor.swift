66079091891eb99565d2af41061a8ec3e41570fb
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2020-04-10 22:30:00 +0200

    Initial implementarion

--- Sources/EventMonitor/EventMonitor.swift
+++ Sources/EventMonitor/EventMonitor.swift
@@ -1,3 +1,60 @@
-struct EventMonitor {
-    var text = "Hello, World!"
+import Cocoa
+
+class EventMonitor {
+    enum MonitorType {
+        case global
+        case local
+    }
+
+    typealias GlobalEventHandler = (NSEvent?) -> Void
+    typealias LocalEventHandler = (NSEvent?) -> NSEvent?
+
+    private var monitor: Any?
+    private let mask: NSEvent.EventTypeMask
+    private let monitorType: MonitorType
+    private let globalHandler: GlobalEventHandler?
+    private let localHandler: LocalEventHandler?
+
+    init(monitorType: MonitorType, mask: NSEvent.EventTypeMask, globalHandler: GlobalEventHandler?, localHandler: LocalEventHandler?) {
+        self.mask = mask
+        self.monitorType = monitorType
+        self.globalHandler = globalHandler
+        self.localHandler = localHandler
+    }
+
+    deinit {
+        stop()
+    }
+
+    func start() {
+        switch monitorType {
+        case .global:
+            startGlobalMonitor()
+        case .local:
+            startLocalMonitor()
+        }
+    }
+
+    func stop() {
+        guard let monitor = self.monitor else { return }
+
+        NSEvent.removeMonitor(monitor)
+        self.monitor = nil
+    }
+
+    private func startGlobalMonitor() {
+        guard let handler = globalHandler else {
+            assertionFailure("Global event handler is not set.")
+            return
+        }
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
+    }
+
+    private func startLocalMonitor() {
+        guard let handler = localHandler else {
+            assertionFailure("Local event handler is not set.")
+            return
+        }
+        monitor = NSEvent.addLocalMonitorForEvents(matching: mask, handler: handler)
+    }
 }
--- Tests/EventMonitorTests/EventMonitorTests.swift
+++ Tests/EventMonitorTests/EventMonitorTests.swift
@@ -6,7 +6,6 @@ final class EventMonitorTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(EventMonitor().text, "Hello, World!")
     }
 
     static var allTests = [
