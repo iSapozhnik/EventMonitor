bdc9ef02a3ab465af9b8eb932459fa5517eaf1d5
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2020-04-10 22:37:59 +0200

    Access level

--- Sources/EventMonitor/EventMonitor.swift
+++ Sources/EventMonitor/EventMonitor.swift
@@ -1,13 +1,13 @@
 import Cocoa
 
-class EventMonitor {
-    enum MonitorType {
+public class EventMonitor {
+    public enum MonitorType {
         case global
         case local
     }
 
-    typealias GlobalEventHandler = (NSEvent?) -> Void
-    typealias LocalEventHandler = (NSEvent?) -> NSEvent?
+    public typealias GlobalEventHandler = (NSEvent?) -> Void
+    public typealias LocalEventHandler = (NSEvent?) -> NSEvent?
 
     private var monitor: Any?
     private let mask: NSEvent.EventTypeMask
@@ -15,7 +15,7 @@ class EventMonitor {
     private let globalHandler: GlobalEventHandler?
     private let localHandler: LocalEventHandler?
 
-    init(monitorType: MonitorType, mask: NSEvent.EventTypeMask, globalHandler: GlobalEventHandler?, localHandler: LocalEventHandler?) {
+    public init(monitorType: MonitorType, mask: NSEvent.EventTypeMask, globalHandler: GlobalEventHandler?, localHandler: LocalEventHandler?) {
         self.mask = mask
         self.monitorType = monitorType
         self.globalHandler = globalHandler
@@ -26,7 +26,7 @@ class EventMonitor {
         stop()
     }
 
-    func start() {
+    public func start() {
         switch monitorType {
         case .global:
             startGlobalMonitor()
@@ -35,7 +35,7 @@ class EventMonitor {
         }
     }
 
-    func stop() {
+    public func stop() {
         guard let monitor = self.monitor else { return }
 
         NSEvent.removeMonitor(monitor)
